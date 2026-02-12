c985f33d9b0a0756169a6d8f2e42b2bfcbd5ea8f
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-02-12 16:16:54 +0100

    complete mileston 2

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
-const nameSurname= document.getElementById("nome_cognome")
+const formEl = document.querySelector("form")
+console.log(formEl);
+
+let nameSurname = document.getElementById("nome_cognome")
 /* console.log(nameSurname); */
-const Kilometers= document.getElementById("km")
+let kilometers = document.getElementById("km")
 /* console.log(Kilometers); */
-const ageEl= document.getElementById("age")
+let ageEl = document.getElementById("age")
 /* console.log(ageEl); */
-const buttonGen= document.querySelector(".btn_generate")
+let buttonGen = document.querySelector(".btn_generate")
 /* console.log(buttonGen); */
-const buttonStop= document.querySelector(".btn_stop")
+let buttonStop = document.querySelector(".btn_stop")
 /* console.log(buttonStop); */
+let totalPrice = document.getElementById("total")
+
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log(nameSurname.value);
+    console.log(kilometers.value)
+    let discount = 0
+    let kmValue = parseInt(kilometers.value)
+    let price = (kmValue * 0.21)
+    if (ageEl.value==="minor") {
+        discount = .20
+    } else if (ageEl.value==="over65") {
+        discount = .40
+    } else {
+        discount = 0
+    }
+    total = (price - (price * discount))
+    console.log(total.toFixed(2));
+    totalPrice.innerHTML=total.toFixed(2)
+})
+
+buttonStop.addEventListener("click", (event) => {
+    event.preventDefault()
+    nameSurname.value=" "
+    kilometers.value=" "
+    totalPrice.innerHTML="0000"
+    console.log(totalPrice);
+    
+})
